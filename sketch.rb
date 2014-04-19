15318681477a8cf61ae041f449c11c0f7e9797b8
Emil Soman <emil.soman@gmail.com>
2014-04-20 02:23:41 +0530

    Draw rocket shape

--- sketch.rb
+++ sketch.rb
@@ -1,9 +1,30 @@
 def setup
   size(1000, 600)
+  @size = 30
 end
 
 def draw
   background(255)
+  fill(200, 100)
+  stroke(0)
+  stroke_weight(1)
+  push_matrix
+
+  translate(width/2, height/2)
+
+  # Thrusters
+  rect_mode(CENTER)
   fill(0)
-  ellipse(width/2, height/2, 50, 100)
+  rect(-@size/2, @size*2, @size/2, @size)
+  rect(@size/2, @size*2, @size/2, @size)
+
+  # Rocket body
+  fill(175)
+  begin_shape(TRIANGLES)
+  vertex(0, -@size*2)
+  vertex(-@size, @size*2)
+  vertex(@size, @size*2)
+  end_shape
+
+  pop_matrix
 end
